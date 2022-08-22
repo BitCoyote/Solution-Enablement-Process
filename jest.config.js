ed9525b995e85054bd9091cbeb3bce19b144d8bf
Jordan Caudill <jordan.caudill@chaione.com>
2022-08-22 15:57:30 -0500

    Updated jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,30 @@
 module.exports = {
-    transform: {
-      '^.+\\.(t|j)sx?$': 'ts-jest'
+  transform: {
+    '^.+\\.(t|j)sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+  ],
+  coverageDirectory: '<rootDir>/coverage/jest',
+  coverageReporters: [
+    'json',
+    'lcov',
+    'text',
+    'clover',
+    'cobertura',
+  ],
+  reporters: [
+    'default',
+    'jest-junit',
+  ],
+  clearMocks: true,
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json',
     },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    coverageDirectory: '<rootDir>/coverage/jest',
-    coverageReporters: ["json", "lcov", "text", "clover", "cobertura"],
-    reporters: [
-      'default',
-      'jest-junit'
-    ],
-    clearMocks: true,
-    globals: {
-      'ts-jest': {
-        tsConfig: 'tsconfig.test.json'
-      }
-    }
-  };
+  },
+  preset: 'ts-jest/presets/js-with-ts',
+}
